64fdf2a18c538ebf06bc2f6067da3e2cde652bcf
Dmitry Kazarov <d.kazarov1361@gmail.com>
2022-05-25 14:28:11 +0300

    add serch input

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import { Header } from './components/Header';
-import Home from './pages/Home';
 import { Routes, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
 import { NotFound } from './pages/NotFound';
 import { Cart } from './pages/Cart';
+
+import { Header } from './components/Header';
 
 import './scss/app.scss';
 
 const App = () => {
+  const [searchValue, setSearchValue] = React.useState('');
+
   return (
     <div className='wrapper'>
-      <Header />
+      <Header searchValue={searchValue} setSearchValue={setSearchValue} />
       <div className='content'>
         <Routes>
           <Route path='/' element={<Home />} />
