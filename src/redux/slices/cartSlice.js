df28bf9d1eb63f932c6431178698430d550c2193
Dmitry Kazarov <d.kazarov1361@gmail.com>
2022-06-08 16:26:34 +0300

    add +/- in cart

--- src/redux/slices/cartSlice.js
+++ src/redux/slices/cartSlice.js
@@ -9,10 +9,6 @@ export const cartSlice = createSlice({
   name: 'cart',
   initialState,
   reducers: {
-    // addItem(state, action) {
-    //   state.items.push(action.payload);
-    //   state.totalPrice = state.items.reduce((sum, obj) => obj.price + sum, 0);
-    // },
     addItem(state, action) {
       const findItem = state.items.find((obj) => obj.id === action.payload.id);
       if (findItem) {
@@ -21,10 +17,24 @@ export const cartSlice = createSlice({
         state.items.push({ ...action.payload, count: 1 });
       }
 
-      state.totalPrice = state.items.reduce((sum, obj) => obj.price * obj.count + sum, 0);
+      state.totalPrice = state.items.reduce((sum, obj) => {
+        return obj.price * obj.count + sum;
+      }, 0);
     },
     removeItem(state, action) {
       state.items.filter((obj) => obj.id !== action.payload);
+    },
+    // incrementItem(state, action) {
+    //   const findItem = state.items.find((obj) => obj.id === action.payload);
+    //   if (findItem) {
+    //     findItem.count++;
+    //   }
+    // },
+    decrementItem(state, action) {
+      const findItem = state.items.find((obj) => obj.id === action.payload);
+      if (findItem) {
+        findItem.count--;
+      }
     },
     clearItems(state) {
       state.items = [];
@@ -33,6 +43,6 @@ export const cartSlice = createSlice({
 });
 
 // Action creators are generated for each case reducer function
-export const { addItem, removeItem, clearItems } = cartSlice.actions;
+export const { addItem, removeItem, clearItems, decrementItem } = cartSlice.actions;
 
 export default cartSlice.reducer;
